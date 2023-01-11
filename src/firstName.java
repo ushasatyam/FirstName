8cb1d8f79733d01ec81e97d3e93ca41f7543603c
ushasatyam <ushasatyam@gmail.com>
2023-01-11 01:49:44 -0600

    initial commition 10 jan 2023

---
+++ src/firstName.java
@@ -0,0 +1,33 @@
+import java.nio.file.FileSystemNotFoundException;
+
+/**
+ * 
+ */
+
+/**
+ * @author ushas
+ *
+ */
+public class firstName {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stu
+		
+		
+		System.out.println("usha");
+		
+		System.out.println("Arshu");
+		
+		System.out.println("Chikku");
+		
+		
+		
+		
+
+	}
+
+}
